7580d5127e733b9b9de9627e01735d0bd38de8b5
Bethany Ryalls <bethany@motifdesignstudio.com>
2022-12-16 19:24:35 +0000

    adding variables and displaying total months and using for loop to calculate net total

--- index.js
+++ index.js
@@ -86,3 +86,44 @@ var finances = [
 ['Jan-2017', 138230],
 ['Feb-2017', 671099]
 ];
+
+var totalMonths = finances.length;
+var total = 0;
+var biggestIncrease = [0 , 0];
+var biggestDecrease = [0 , 0];
+var change;
+var netTotal = 0;
+
+// The total number of months included in the dataset.
+
+console.log("Total number of months included in the dataset: " + totalMonths);
+
+// The net total amount of Profit/Losses over the entire period.
+
+for (let index = 0; index < finances.length; index++) {
+    netTotal += finances[index][1];
+}
+
+console.log("Net total amount of Profit/Losses over the entire period: $" + netTotal);
+
+// add everything together
+
+// probably a loop
+// The average of the changes in Profit/Losses over the entire period.
+// calculate each change by subtracting the previous month from this month
+// You will need to track what the total change in profits is from month to month and then find the average.
+// (Total/total number of changes) ===> total change/(months - 1)
+// maybe put all the changes into an array? using .push(...) ?
+// The greatest increase in profits (date and amount) over the entire period.
+// start with 0
+//   check the last increase. If it's bigger than 0, keep track of the new biggest one.
+//   in a loop
+// The greatest decrease in losses (date and amount) over the entire period.
+// console output format!
+// Financial Analysis
+// ----------------------------
+// Total Months: 25
+// Total: $2561231
+// Average  Change: $-2315.12
+// Greatest Increase in Profits: Feb-2012 ($1926159)
+// Greatest Decrease in Profits: Sep-2013 ($-2196167)
